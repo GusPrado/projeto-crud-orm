245d1d1a9920331e73a974c4c290aa3e91cf0402
Gus Prado <gusgprado@outlook.com>
2019-05-15 17:24:30 -0300

    Initial commit

---
+++ models/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize('cadastro-orm', 'root', '', {
+    dialect: 'mysql',
+    host: '127.0.0.1'
+})
+
+//sequelize.authenticate().then(() => console.log('auth'))
+
+const models = {}
+const fs = require('fs')
+const path = require('path')
+fs
+    .readdirSync(__dirname)
+    .filter((file) => file !== 'index.js')
+    .forEach((file) => {
+        const model = sequelize.import(path.join(__dirname, file))
+        models[model.name] = model
+    })
+
+
+module.exports = {
+        sequelize,
+        models
+    }
+    //sequelize.sync().then(() => console.log('synced'))
